08e26cc15b2f208399e14fad4f113aba7b2fce38
SobyUmell <69790478+SobyUmell@users.noreply.github.com>
2022-11-26 18:31:41 +0300

    Hey dude, how are you?

--- frontend/src/pages/form/components/Form2.js
+++ frontend/src/pages/form/components/Form2.js
@@ -6,7 +6,7 @@ import img2 from '../img/img2.png'
 export default function Form2() {
   return (
     <div className='form2'>
-      <h3 className='form2__title'>Choose the city where you work in?</h3>
+      <h3 className='form2__title-main'>Choose the city where you work in?</h3>
       <ul className='form2__list'>
         <li className='form2__item'>
           <div className='form2__img'>
